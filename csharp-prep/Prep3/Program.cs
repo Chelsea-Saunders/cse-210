827eb91dcd10a77810984ac80e12b2c2ef4dc5fc
Chelsea-Saunders <168315598+Chelsea-Saunders@users.noreply.github.com>
2024-09-24 11:11:05 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,61 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random random = new Random();
-        bool playAgain = true;
-        int min = 1;
-        int max = 100;
-        int guess;
-        int number;
-        int guesses;
-        string response;
-
-        while(playAgain)
-        {
-            guess = 0;
-            guesses = 0;
-            response = "";
-            number = random.Next(min, max + 1);
-
-            while(guess != numnber)
-            {
-                console.WriteLine ("Guess a number between" + min + " - " + max + ":");
-                guess = convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Guess: " + guess);
-
-                if (guess > number)
-                {
-                    Console.WriteLine(guess + " is to high!");
-                }
-                else if (guess < number)
-                {
-                    Console.WriteLine(guess + " is to low!");
-                }
-                guesses++;
-
-            }
-
-            Console.WriteLine("Number: " + number);
-            Console.WriteLine("YOU WIN!!")
-            Console.WriteLine("Guesses: " + guesses); 
-
-            Console.WriteLine("Would you like to play again (Y/N): ")
-            response = console.ReadLine();
-            response = response.ToUpper();
-
-            if (response == "Y")
-            {
-                playAgain = true;
-            }
-            else
-            {
-                playAgain = false;
-            }
-
-        }
-
-        Console.WriteLine("Thanks for playing! ... I guess");
-
+       
         Console.ReadyKey();
 
     }
